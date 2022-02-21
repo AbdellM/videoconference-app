ed3f3d81fe4fdc843accd89cf487ae123b949849
AbdellM <abdelmonim.dakkon@gmail.com>
2022-02-21 20:39:24 +0100

    Fontend setup

--- src/server.js
+++ src/server.js
@@ -2,5 +2,12 @@ import express from "express"
 
 const app = express()
 
-console.log('Hello');
-app.listen(3000)
+app.set("view engine", "pug")
+app.set("views",__dirname + "/views")
+app.use("/public", express.static(__dirname + '/public'));
+
+
+app.get("/", (req, res) => res.render("home"))
+
+console.log('Listening on port http://localhost:3001/')
+app.listen(3001)

7c1baae79dce09c232bb3f12d7f3c320ccaae4d3
AbdellM <abdelmonim.dakkon@gmail.com>
2022-02-22 11:53:36 +0100

    WebSocket server setup

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
-import express from "express"
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
-const app = express()
+const app = express();
 
-app.set("view engine", "pug")
-app.set("views",__dirname + "/views")
-app.use("/public", express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => res.render("home"));
 
-app.get("/", (req, res) => res.render("home"))
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ server });
 
-console.log('Listening on port http://localhost:3001/')
-app.listen(3001)
+server.listen(3000, () => {
+  console.log("Listening on port http://localhost:3000/");
+});

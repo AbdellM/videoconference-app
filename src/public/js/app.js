fedb9cbba9deb85ea7c9c22ffacb1728d8c90966
AbdellM <abdelmonim.dakkon@gmail.com>
2022-02-23 23:48:28 +0100

    SocketIO server setup ✔

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,36 +1,11 @@
-const socketConnection = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nicknameForm = document.querySelector("#nickname");
+const welcome = document.querySelector("#welcome");
+const form = welcome.querySelector("form");
 
-socketConnection.addEventListener("open", () => {
-  console.log("Connected to the server ✅");
-});
-
-socketConnection.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-socketConnection.addEventListener("close", () => {
-  console.log("Disconnected from the server ❌");
-});
-
-messageForm.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socketConnection.send(
-    JSON.stringify({ type: "message", payload: input.value })
-  );
+  const input = form.querySelector("input");
+  socket.emit("room", { payload: input.value });
   input.value = "";
-});
-
-nicknameForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const input = nicknameForm.querySelector("input");
-  socketConnection.send(
-    JSON.stringify({ type: "nickname", payload: input.value })
-  );
 });
